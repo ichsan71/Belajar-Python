314cee374488bf7c6b77ab61f790ba3a651f6ee6
ichsan71 <ichsan.kamil71@gmail.com>
2020-10-24 00:24:33 +0700

    Belajar Input User.py

---
+++ latihan5/inputuser.py
@@ -0,0 +1,19 @@
+# Input user
+
+
+# data yang dimasukan pasti string
+data = input("Masukan data :")
+
+print("data = ",data,", type = ",type(data))
+
+# jika kita ingin mengambil int, maka
+angka = int(input("Masukan Angka :"))
+angka = float(input("Masukan Angka :"))
+print("angka = ",angka,", type = ",type(angka))
+
+# bagaimana dengan boolean
+biner = bool(int(input("Masukan Nilai :")))
+print("biner = ",biner,", type = ",type(biner))
+
+
+
