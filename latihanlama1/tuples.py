d0f50f2d712f301bb7e985815435ba02daa9846a
ichsan71 <ichsan.kamil71@gmail.com>
2020-10-31 19:25:52 +0700

    belajar SKcoding Tuples.py

---
+++ latihanlama1/tuples.py
@@ -0,0 +1,12 @@
+# Belajar Tuples ()
+# ini hampir sama dengan list
+# hanya saja tuples imutable
+# gk bisa edit,delete,add
+
+
+orang = ('ichsan','ntvrmlyn','koesming')
+print(orang)
+print(orang[2])
+
+# Convert tuples ke List
+print(list(orang))
